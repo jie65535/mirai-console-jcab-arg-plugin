5a34a6cc749d95a81b76c400dff2e46d6c90db5b
jie65535 <jie65535@qq.com>
2022-04-07 21:51:00 +0800

    更新版本到v1.2.0 增加种子评分与评价 感谢群友 @西宏 @萧天辰 等贡献评分

--- src/main/kotlin/JAlchemyRecipeGenerator.kt
+++ src/main/kotlin/JAlchemyRecipeGenerator.kt
@@ -15,7 +15,7 @@ object JAlchemyRecipeGenerator : KotlinPlugin(
     JvmPluginDescription(
         id = "top.jie65535.mirai-console-jcab-arg-plugin",
         name = "Create: Above and Beyond 炼金配方生成器",
-        version = "1.0",
+        version = "1.2.0",
     ) {
         author("jie65535")
         info("Create: Above and Beyond 整合包\n炼金配方生成器，输入种子生成配方")
@@ -65,8 +65,16 @@ object JAlchemyRecipeGenerator : KotlinPlugin(
 //                sb.appendLine(']')
         }
         // 硅
-        sb.append("硅反应物：混沌催化剂 + ${transmutation.transmuted(Silicon).name}反应物\n")
-        sb.append("银反应物：混沌催化剂 + ${transmutation.transmuted(Silver).name}反应物")
+        val siliconReactant = transmutation.transmuted(Silicon).name
+        sb.appendLine("硅反应物：混沌催化剂 + ${siliconReactant}反应物")
+        sb.appendLine("银反应物：混沌催化剂 + ${transmutation.transmuted(Silver).name}反应物")
+
+        val score = materialScore[siliconReactant] ?: 5
+        sb.append("种子评价【")
+        for (l in 1..5)
+            sb.append(if (l <= score) "★" else "☆")
+        sb.append("】\n评语：").append(scoreComment[score])
+
         return sb.toString()
     }
 
@@ -78,6 +86,30 @@ object JAlchemyRecipeGenerator : KotlinPlugin(
         Catalyst("金属催化剂",   arrayOf("锌", "铜", "铁", "镍", "铅", "金")),
         Catalyst("宝石催化剂",   arrayOf("朱砂", "青金石", "蓝宝石", "绿宝石", "红宝石", "钻石")),
         Catalyst("混沌催化剂",   arrayOf("火成催化剂", "草本催化剂", "不稳定催化剂", "晶化催化剂", "金属催化剂", "宝石催化剂")),
+    )
+
+    /**
+     * 反应物材料价值表
+     * https://docs.qq.com/sheet/DZndZSVFxSkdnaXh0
+     */
+    private val materialScore = mapOf(
+        "安山岩" to 5,	"闪长岩" to 5,	"花岗岩" to 5,	"圆石" to 5,	"玄武岩" to 5,	"辉长岩" to 5,
+        "绯红" to 5,	"橙色" to 5,	"黄色" to 5,	"绿色" to 5,	"蓝色" to 5,	"品红色" to 5,
+        "烈焰" to 3,	"史莱姆" to 4,	"下界" to 5,	"黑曜石" to 5,	"火药" to 5,	"海晶" to 3,
+        "神秘" to 1,	"磷灰石" to 4,	"硫磺" to 4,	"硝石" to 4,	"赛特斯石英" to 3,	"下界石英" to 3,
+        "锌" to 4,	"铜" to 4,	"铁" to 4,	"镍" to 3,	"铅" to 3,	"金" to 2,
+        "朱砂" to 3,	"青金石" to 3,	"蓝宝石" to 1,	"绿宝石" to 1,	"红宝石" to 1,	"钻石" to 1,
+    )
+
+    /**
+     * 评分评语
+     */
+    private val scoreComment = mapOf(
+        1 to "逆大天，这破种子赶紧remake吧",
+        2 to "寄了",
+        3 to "不大行",
+        4 to "还不错",
+        5 to "这运气没谁了，吃点好的吧",
     )
 
     /**
